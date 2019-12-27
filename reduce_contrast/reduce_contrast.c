4ad7bf5657cfbccde46552798c888dc89e007104
psawicki0 <01143606@pw.edu.pl>
2019-12-27 04:10:56 +0100

    added rc

--- reduce_contrast/reduce_contrast.c
+++ reduce_contrast/reduce_contrast.c
@@ -15,6 +15,7 @@ int main(int argc, char **argv){
 	fseek(f, pixelDataOffset, SEEK_SET);
 	int pixs = 3 * imageHeight * (imageWidth + imageWidth % 4);
 	rfactor /= 128;
+	rfactor = 1 - rfactor;
 	unsigned int j = 0;
 	do{
 		fseek(f, pixelDataOffset + j, SEEK_SET);
@@ -22,12 +23,8 @@ int main(int argc, char **argv){
 		unsigned int toRead = pixs < 256 ? pixs : 256;
 		fread(pixels, 1, toRead, f);
 		unsigned int i = 0;
-		for(; i < toRead; ++i){
-			if(pixels[i] > 128)
-				pixels[i] = rfactor * (pixels[i] - 128) + 128;	
-			else
-				pixels[i] = 128;
-		}
+		for(; i < toRead; ++i)
+			pixels[i] = rfactor * (pixels[i] - 128) + 128;
 		fseek(f, pixelDataOffset + j, SEEK_SET);
 		fwrite(pixels, 1, toRead, f);
 		pixs -= toRead;
